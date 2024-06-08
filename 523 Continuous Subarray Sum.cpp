200dcca67d2019ced905456bf51f96e65a01161f
Ryoukiwei <dodoinass2@gmail.com>
2024-06-08 09:46:05 +0800

    20240608: algo 2

--- 523 Continuous Subarray Sum.cpp
+++ 523 Continuous Subarray Sum.cpp
@@ -1,33 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// class Solution
+// {
+// public:
+//     bool checkSubarraySum(vector<int> &nums, int k)
+//     {
+//         const int n = nums.size();
+//         if (n < 2)
+//             return false;
+
+//         int sum = 0;
+//         unordered_map<int, int> idx{{0, -1}};
+
+//         for (int i = 0; i < n; ++i)
+//         {
+//             sum += nums[i];
+
+//             if (k)
+//                 sum %= k;
+
+//             const auto it = idx.find(sum);
+//             if (it != idx.cend())
+//             {
+//                 if (i - it->second >= 2)
+//                     return true;
+//             }
+//             else
+//                 idx[sum] = i;
+//         }
+
+//         return false;
+//     }
+// };
+
 class Solution
 {
 public:
     bool checkSubarraySum(vector<int> &nums, int k)
     {
-        const int n = nums.size();
-        if (n < 2)
+        if (nums.size() < 2)
             return false;
 
-        int sum = 0;
-        unordered_map<int, int> idx{{0, -1}};
+        auto begin = nums.begin(), end = nums.end();
+        auto l = begin, r = next(begin);
+        int sum = *l;
 
-        for (int i = 0; i < n; ++i)
+        while (r != end)
         {
-            sum += nums[i];
+            sum += *r;
 
-            if (k)
-                sum %= k;
+            if (!(sum % k))
+                return true;
 
-            const auto it = idx.find(sum);
-            if (it != idx.cend())
+            if (*r == *prev(r) && !(*r))
+                return true;
+
+            int temp = sum;
+            l = begin;
+            while ((r - l) > 1 && temp >= k)
             {
-                if (i - it->second >= 2)
+                temp -= *l++;
+
+                if (!(temp % k))
                     return true;
             }
-            else
-                idx[sum] = i;
+
+            ++r;
         }
 
         return false;
