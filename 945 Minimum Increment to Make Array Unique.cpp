a2bc60facce4fb8f3ad83096646f006e43157250
Ryoukiwei <dodoinass2@gmail.com>
2024-06-15 02:13:31 +0800

    20240615: algo 2

--- 945 Minimum Increment to Make Array Unique.cpp
+++ 945 Minimum Increment to Make Array Unique.cpp
@@ -1,21 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// class Solution
+// {
+// public:
+//     int minIncrementForUnique(vector<int> &nums)
+//     {
+//         int ans = 0;
+//         sort(nums.begin(), nums.end());
+//         auto it = next(nums.begin()), end = nums.end();
+
+//         for (it; it != end; ++it)
+//         {
+//             if (!(*prev(it) < *it))
+//             {
+//                 ans += *prev(it) - *it + 1;
+//                 *it = *prev(it) + 1;
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
 class Solution
 {
 public:
     int minIncrementForUnique(vector<int> &nums)
     {
+        auto minmax = minmax_element(nums.begin(), nums.end());
+        const int n = nums.size(), MIN = *minmax.first, MAX = *minmax.second;
+
+        vector<int> freq(MAX - MIN + n + 1);
+
+        for (const int num : nums)
+        {
+            ++freq[num - MIN];
+        }
+
+        auto it = freq.begin(), end = freq.end();
         int ans = 0;
-        sort(nums.begin(), nums.end());
-        auto it = next(nums.begin()), end = nums.end();
 
         for (it; it != end; ++it)
         {
-            if (!(*prev(it) < *it))
+            if (*it > 0)
             {
-                ans += *prev(it) - *it + 1;
-                *it = *prev(it) + 1;
+                const int extra = *it - 1;
+                *next(it) += extra;
+                ans += extra;
             }
         }
 
