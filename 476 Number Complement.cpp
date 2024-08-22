5a10a17309b2db0bebdb076b7d1386daf6f95af8
Ryoukiwei <dodoinass2@gmail.com>
2024-08-22 18:34:01 +0800

    20240822: solved

---
+++ 476 Number Complement.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findComplement(int num)
+    {
+        for (long i = 1; i <= num; i <<= 1)
+        {
+            num ^= i;
+        }
+
+        return num;
+    }
+};
