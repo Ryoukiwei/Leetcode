4b68b1e4afab310bffb1ffb8c051993003a7bdde
Ryoukiwei <dodoinass2@gmail.com>
2025-01-11 23:05:34 +0800

    20250111: solved

---
+++ 1400 Construct K Palindrome Strings.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <bitset>
+using namespace std;
+
+class Solution
+{
+public:
+    bool canConstruct(string s, int k)
+    {
+        if (s.length() < k)
+            return false;
+
+        bitset<26> odd;
+
+        for (const char c : s)
+        {
+            odd.flip(c - 'a');
+        }
+
+        return odd.count() <= k;
+    }
+};
